f5cf3cd65cec6c97c515efb424ce51eff6bdf6c1
robinharms <robin@betahaus.net>
2015-09-01 13:52:38 +0200

    Made it easier to register strings + using awesome-sluggify to normalize them if needed.

--- arche_ttw_translation/models.py
+++ arche_ttw_translation/models.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
 from UserDict import IterableUserDict
 
-from arche.interfaces import IRoot
 from BTrees.OOBTree import OOBTree
+from arche.interfaces import IRoot
+from pyramid.threadlocal import get_current_request
+from slugify import Slugify
 from zope.component import adapter
 from zope.interface import implementer
 
@@ -11,6 +13,20 @@ from arche_ttw_translation.interfaces import ITranslations
 
 _marker = object()
 
+
+class Translatable(IterableUserDict):
+
+    def __call__(self, key, default = None):
+        self[key] = default
+
+    def __setitem__(self, key, value):
+        if not value:
+            value = key
+        slugger = Slugify()
+        key = slugger(key)
+        self.data[key] = value
+
+
 @adapter(IRoot)
 @implementer(ITranslations)
 class Translations(IterableUserDict):
@@ -18,7 +34,9 @@ class Translations(IterableUserDict):
     def __init__(self, context):
         self.context = context
 
-    def __call__(self, txt, request, lang = None, default = _marker):
+    def __call__(self, txt, request = None, lang = None, default = _marker):
+        if request == None and lang == None:
+            request = get_current_request()
         lang = lang and lang or request.localizer.locale_name
         default = default == _marker and txt or default
         return self.get(lang, {}).get(txt, default)
@@ -38,7 +56,7 @@ class Translations(IterableUserDict):
 
 
 def register_ttwt(config, translations):
-    assert isinstance(translations, dict)
+    assert isinstance(translations, Translatable)
     if not hasattr(config.registry, '_ttwt'):
         config.registry._ttwt = []
     config.registry._ttwt.append(translations)
@@ -49,6 +67,10 @@ def get_registered_ttwt(request):
         results.update(translations)
     return results
 
+def ttwt(request):
+    return ITranslations(request.root)
+
 def includeme(config):
     config.registry.registerAdapter(Translations)
     config.add_directive('register_ttwt', register_ttwt)
+    config.add_request_method(ttwt, reify = True)
